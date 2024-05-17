b4ce188e037d6c88166f6c47651f4ea1d4e69d7d
shandriy <entyspite@gmail.com>
2024-05-16 20:45:05 -0700

    Added python filebuild script for JS

---
+++ build.py
@@ -0,0 +1,23 @@
+import os
+import glob
+
+directory = r"./append/**"
+directoryList = glob.glob(directory, recursive=True)
+
+mainFile = open("main.js", "w")
+mainFile.write("")
+mainFile.close()
+mainFile = open("main.js", "a")
+
+for fileName in directoryList:
+  filePath = fileName
+  print(filePath)
+
+  if os.path.isfile(filePath):
+    file = open(filePath, "r")
+    fileContents = file.read()
+    file.close()
+
+    mainFile.write("\n" + fileContents + "\n")
+
+mainFile.close()
