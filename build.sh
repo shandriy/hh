f7737d9c3e05a315cfbf890e09e0e8774501b7cd
shandriy <entyspite@gmail.com>
2024-05-18 21:43:30 -0700

    Made tsconfig autodelete

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ done < "discard"
 echo "]}" >> tsconfig.json
 
 npx tsc -t es5 -p tsconfig.json
+rm tsconfig.json
 
 uglifyjs main.js -m reserved=[gen] --toplevel > discard
 echo "/*! Copyright (C) Konstantin \"entyspite\" Edunov 2024 */" > main.js
