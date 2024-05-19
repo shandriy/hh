43a9137afd2ef032266b74fdb793ce00793c0f31
shandriy <entyspite@gmail.com>
2024-05-18 21:48:37 -0700

    Reformatted

--- append/generic/generic.js
+++ append/generic/generic.js
@@ -6,61 +6,66 @@ var gen = {
 
   deltaTime: 13,
   lastFrame: 0,
-  frame: function() {
-    var now = Date.now();
-    gen.deltaTime = now - gen.lastFrame;
-    gen.lastFrame = now;
-  },
-  init: function() {
-    try {
-      document.getElementsByTagName("p")[0].remove();
-      document.getElementsByTagName("noscript")[0].remove();
-    } catch (error) {
-      document.getElementsByTagName("p")[0].innerHTML = "";
+
+  statusChecker: {
+    names: [],
+    status: []
+  }
+};
+gen.frame = function() {
+  var now = Date.now();
+  gen.deltaTime = now - gen.lastFrame;
+  gen.lastFrame = now;
+};
+gen.init = function() {
+  try {
+    document.getElementsByTagName("p")[0].remove();
+    document.getElementsByTagName("noscript")[0].remove();
+  } catch (error) {
+    document.getElementsByTagName("p")[0].innerHTML = "";
+  };
+
+  var aspectRatio = out.canvas.aspectRatio;
+
+  gen.canvas = document.getElementsByTagName("canvas")[0];
+  gen.context = gen.canvas.getContext("2d", { alpha: false });
+  function onResize() {
+    var screenWidth = window.innerWidth;
+    var screenHeight = window.innerHeight;
+    var screenAspectRatio = screenWidth / screenHeight;
+
+    var width = out.canvas.unitWidth;
+    var height = out.canvas.unitHeight;
+
+    if (screenAspectRatio < aspectRatio) {
+      width = screenWidth;
+      height = screenWidth / aspectRatio;
+
+      gen.canvas.style.left = "0px";
+      gen.canvas.style.top = (screenHeight - height) / 2 + "px";
+    } else {
+      height = screenHeight;
+      width = screenHeight * aspectRatio;
+
+      gen.canvas.style.top = "0px";
+      gen.canvas.style.left = (screenWidth - width) / 2 + "px";
     };
 
-    var aspectRatio = out.canvas.aspectRatio;
+    width = Math.round(width);
+    height = Math.round(height);
+    gen.canvas.setAttribute("width", width.toString());
+    gen.canvas.setAttribute("height", height.toString());
+    gen.canvas.style.width = width + "px";
+    gen.canvas.style.height = height + "px";
+    gen.context.fillStyle = "#fff";
+    gen.context.fillRect(0, 0, width, height);
 
-    gen.canvas = document.getElementsByTagName("canvas")[0];
-    gen.context = gen.canvas.getContext("2d", { alpha: false });
-    function onResize() {
-      var screenWidth = window.innerWidth;
-      var screenHeight = window.innerHeight;
-      var screenAspectRatio = screenWidth / screenHeight;
-
-      var width = out.canvas.unitWidth;
-      var height = out.canvas.unitHeight;
-
-      if (screenAspectRatio < aspectRatio) {
-        width = screenWidth;
-        height = screenWidth / aspectRatio;
-
-        gen.canvas.style.left = "0px";
-        gen.canvas.style.top = (screenHeight - height) / 2 + "px";
-      } else {
-        height = screenHeight;
-        width = screenHeight * aspectRatio;
-
-        gen.canvas.style.top = "0px";
-        gen.canvas.style.left = (screenWidth - width) / 2 + "px";
-      };
-
-      width = Math.round(width);
-      height = Math.round(height);
-      gen.canvas.setAttribute("width", width.toString());
-      gen.canvas.setAttribute("height", height.toString());
-      gen.canvas.style.width = width + "px";
-      gen.canvas.style.height = height + "px";
-      gen.context.fillStyle = "#fff";
-      gen.context.fillRect(0, 0, width, height);
-
-      out.canvas.width = width;
-      out.canvas.height = height;
-    }
-    window.addEventListener("resize", onResize);
-    window.addEventListener("focus", onResize);
-    onResize();
-
-    window.setInterval(gen.frame);
+    out.canvas.width = width;
+    out.canvas.height = height;
   }
+  window.addEventListener("resize", onResize);
+  window.addEventListener("focus", onResize);
+  onResize();
+
+  window.setInterval(gen.frame);
 };
--- append/output/output.js
+++ append/output/output.js
@@ -7,17 +7,17 @@ var out = {
     aspectRatio: 5 / 3,
     width: 500,
     height: 300,
-
-    image: function(image, x, y, scaleX, scaleY) {
-      var convertX = out.canvas.unitWidth * out.canvas.width;
-      var convertY = out.canvas.unitHeight * out.canvas.height;
-      gen.context.drawImage(
-        image,
-        Math.round((x + (out.canvas.unitWidth / 2)) / convertX),
-        Math.round((y + (out.canvas.unitHeight / 2)) / convertY),
-        image.width * scaleX / convertX,
-        image.height * scaleY / convertY
-      );
-    }
   }
 };
+
+out.canvas.image = function(image, x, y, scaleX, scaleY) {
+  var convertX = out.canvas.unitWidth * out.canvas.width;
+  var convertY = out.canvas.unitHeight * out.canvas.height;
+  gen.context.drawImage(
+    image,
+    Math.round((x + (out.canvas.unitWidth / 2)) / convertX),
+    Math.round((y + (out.canvas.unitHeight / 2)) / convertY),
+    image.width * scaleX / convertX,
+    image.height * scaleY / convertY
+  );
+}
