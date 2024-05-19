24970a51e108531ff7e104636af0d6a5a9edebf2
shandriy <entyspite@gmail.com>
2024-05-18 20:06:10 -0700

    Updated build script

--- build.sh
+++ build.sh
@@ -1,1 +1,18 @@
-python3 build.py
+find append/* -name *.ts > discard
+echo "{\"compilerOptions\":{\"outFile\":\"main.js\",\"declaration\":false},\"files\":[" > tsconfig.json
+
+while read line
+do
+  echo "\"$line\"," >> tsconfig.json
+done < "discard"
+echo "]}" >> tsconfig.json
+
+npx tsc -t es5 -p tsconfig.json
+
+rm tsconfig.json
+
+uglifyjs main.js -m reserved=[main] --toplevel > discard
+echo "/*! Copyright (C) Konstantin \"entyspite\" Edunov 2024 */" > main.js
+cat discard >> main.js
+
+rm discard
