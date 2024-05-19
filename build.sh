2afae18736de8d8f0ccce88e850db76a82d8dd85
shandriy <entyspite@gmail.com>
2024-05-19 10:09:24 -0700

    Fixed build process to remove "use strict";

--- build.sh
+++ build.sh
@@ -1,15 +1,32 @@
 find append/* -type f  > discard
 
 echo "" > main.js
+count=0
 while read line
 do
-  cp $line copy
-  echo ";" >> copy
-  echo "" >> copy
-  while read contents
-  do
-    echo "$contents" >> main.js
-  done < "copy"
+    cp $line copy
+    echo ";" >> copy
+    echo "" >> copy
+    while read contents
+    do
+        if [[ "$contents" == "\"use strict\";" ]]
+        then
+            if [[ "$count" -eq 0 ]]
+            then
+                count=1
+                echo "$contents" >> main.js
+            fi
+        elif [[ "$contents" == "'use strict';" ]]
+        then
+            if [[ "$count" -eq 0 ]]
+            then
+                count=1
+                echo "$contents" >> main.js
+            fi
+        else
+            echo "$contents" >> main.js
+        fi
+    done < "copy"
 done < "discard"
 
 uglifyjs main.js -m reserved=[gen] --toplevel > discard
@@ -18,3 +35,5 @@ cat discard >> main.js
 
 rm discard
 rm copy
+
+unset count
