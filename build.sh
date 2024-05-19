4bf9d11779fd2abee8236d00de8c42ec66185871
shandriy <entyspite@gmail.com>
2024-05-18 21:57:38 -0700

    Updated build process

--- build.sh
+++ build.sh
@@ -1,17 +1,20 @@
 find append/* -type f  > discard
-echo "{\"compilerOptions\":{\"outFile\":\"main.js\",\"declaration\":false,\"allowJs\":true},\"files\":[" > tsconfig.json
 
+echo "" > main.js
 while read line
 do
-  echo "\"$line\"," >> tsconfig.json
+  cp $line copy
+  echo ";" >> copy
+  echo "" >> copy
+  while read contents
+  do
+    echo "$contents" >> main.js
+  done < "copy"
 done < "discard"
-echo "]}" >> tsconfig.json
-
-npx tsc -t es5 -p tsconfig.json
-rm tsconfig.json
 
 uglifyjs main.js -m reserved=[gen] --toplevel > discard
 echo "/*! Copyright (C) Konstantin \"entyspite\" Edunov 2024 */" > main.js
 cat discard >> main.js
 
 rm discard
+rm copy
